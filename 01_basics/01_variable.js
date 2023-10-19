51bc3a2c5b21a19a6ebecbd2b4354e16147f555d
Vishal <9512278k@gmail.com>
2023-10-19 11:36:29 +0530

    variables completed

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,6 +3,15 @@ let accountEmail = "vky@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
-accountId = 2
+//accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "2131313"
+accountCity = "Bangalore"
+let accountState;
 
-console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
