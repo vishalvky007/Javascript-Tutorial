3c0b87d270cccae0b302277eb56c9207afdd5fba
Vishal <9512278k@gmail.com>
2023-10-19 17:30:26 +0530

    Comparisons complete

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);  
+
+
+// Never use such type of conversion, avoid them
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (strictly check values and its datatype too)
+
+// console.log("2" === 2);
+
