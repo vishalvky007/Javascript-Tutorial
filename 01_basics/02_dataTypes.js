cd37440355323eddeaf207a4042a07b3e1cfa3d3
Vishal <9512278k@gmail.com>
2023-10-19 11:52:59 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";   // treat all JS code as newer version
+
+// alert("hello")   // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+console.log("Vishal")
+
+let name = "Vishal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned yet
+// symbol 
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
+
+
+
